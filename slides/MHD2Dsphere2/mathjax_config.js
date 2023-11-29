ab9ffa0c9cf9bda940ecd9f82c18e9db4c8cc9cd
nakashima <r.nakashima.a10@m.kyushu-u.ac.jp>
2023-11-29 17:24:03 +0900

    2023 年度後期合同セミナーのスライドを設置

---
+++ slides/MHD2Dsphere2/mathjax_config.js
@@ -0,0 +1,45 @@
+window.MathJax = {
+    loader: {
+        load: ["[tex]/cancel", "[tex]/upgreek"],
+    },
+
+    tex: {
+        packages: {
+            "[+]": ["cancel"],
+            "[+]": ["upgreek"],
+        },
+
+        // インライン数式 //
+        inlineMath: [
+            ["$", "$"],
+            ["\\(", "\\)"],
+        ],
+
+        // ディスプレイ数式 //
+        displayMath: [["\\[", "\\]"]],
+
+        // 自動ナンバリング //
+        tags: "ams",
+
+        macros: {
+            bm: ["{\\boldsymbol{#1}}", 1],
+
+            ip: "{\\boldsymbol{\\cdot}}",
+            grad: "{\\boldsymbol{\\nabla}}",
+            div: "{\\boldsymbol{\\nabla}\\ip}",
+            rot: "{\\boldsymbol{\\nabla}\\times}",
+
+            uppartial: "\\unicode[]{x2202}",
+            upO: "{\\mathrm{O}}",
+            upl: "{\\ell}",
+            upd: "{\\mathrm{d}}",
+            upD: "{\\mathrm{D}}",
+            upe: "{\\mathrm{e}}",
+            upi: "{\\mathrm{i}}",
+        },
+    },
+
+    svg: {
+        fontCache: "global",
+    },
+};
